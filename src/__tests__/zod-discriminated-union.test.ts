e05727fd74973442c12d3b5e00ce432301c7f7e1
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-19 10:28:00 +0100

    error if value isnt an object

---
+++ src/__tests__/zod-discriminated-union.test.ts
@@ -0,0 +1,37 @@
+import { it, describe, expect } from 'vitest'
+
+import { z } from '../schemas'
+
+describe('Discriminated union Schema', () => {
+  it("throws an error if the value isn't an object", () => {
+    const union = z.discriminatedUnion('type', [
+      z.object({
+        type: z.literal('a'),
+        lol: z.number(),
+      }),
+      z.object({
+        type: z.literal('b'),
+        lmao: z.string(),
+      }),
+    ])
+
+    expect(() => union.parse('a')).toThrow('Value must be an object')
+    expect(() => union.parse(1)).toThrow('Value must be an object')
+  })
+
+  //   it('should validate a discriminated union schema', () => {
+  // 	    const union = z.discriminatedUnion('type', [
+  //       z.object({
+  // 	type: z.literal('a'),
+  // 	a: z.number(),
+  //       }),
+  //       z.object({
+  // 	type: z.literal('b'),
+  // 	b: z.string(),
+  //       }),
+  //     ])
+
+  //     expect(union.parse({ type: 'a', a: 1 })).toEqual({ type: 'a', a: 1 })
+  //     expect(union.parse({ type: 'b', b: '1' })).toEqual({ type: 'b', b: '1' })
+  //   })
+})
