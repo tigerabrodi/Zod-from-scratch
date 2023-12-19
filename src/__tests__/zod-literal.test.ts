b2e9e94bbe95c93685a757be98b2e11e3e8a3050
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-19 08:22:31 +0100

    literal value test

--- src/__tests__/zod-literal.test.ts
+++ src/__tests__/zod-literal.test.ts
@@ -6,11 +6,14 @@ describe('Zod literal Schema', () => {
   it('should validate a literal', () => {
     const stringLiteralSchema = z.literal('a')
     expect(stringLiteralSchema.parse('a')).toEqual('a')
+    expect(stringLiteralSchema.value).toBe('a')
 
     const numberLiteralSchema = z.literal(1)
     expect(numberLiteralSchema.parse(1)).toEqual(1)
+    expect(numberLiteralSchema.value).toBe(1)
 
     const booleanLiteralSchema = z.literal(true)
     expect(booleanLiteralSchema.parse(true)).toEqual(true)
+    expect(booleanLiteralSchema.value).toBe(true)
   })
 })
