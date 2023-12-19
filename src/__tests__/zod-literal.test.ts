ec76b83e0063fffff722b77b8a2a3e25a0fb6c2a
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-19 08:23:40 +0100

    literal should throw test

--- src/__tests__/zod-literal.test.ts
+++ src/__tests__/zod-literal.test.ts
@@ -16,4 +16,17 @@ describe('Zod literal Schema', () => {
     expect(booleanLiteralSchema.parse(true)).toEqual(true)
     expect(booleanLiteralSchema.value).toBe(true)
   })
+
+  it("should throw an error if the literal doesn't match", () => {
+    const stringLiteralSchema = z.literal('a')
+    expect(() => stringLiteralSchema.parse('b')).toThrow('Expected a but got b')
+
+    const numberLiteralSchema = z.literal(1)
+    expect(() => numberLiteralSchema.parse(2)).toThrow('Expected 1 but got 2')
+
+    const booleanLiteralSchema = z.literal(true)
+    expect(() => booleanLiteralSchema.parse(false)).toThrow(
+      'Expected true but got false'
+    )
+  })
 })
